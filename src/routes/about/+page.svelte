9ca112ae6a9341aba4ead1334f596fbfd1202e52
commitsovercoffee <commitsovercoffee@gmail.com>
2023-12-09 03:08:42 +0530

    adds about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,85 @@
+<h2>About</h2>
+
+<section class="flex justify-center items-center">
+	<img
+		alt="asdasdad"
+		src="https://picsum.photos/192?random=1"
+		class="p-2 pb-8 bg-white w-52 h-64 object-cover rotate-3 translate-x-3 shadow shadow-darker"
+	/>
+	<img
+		alt="asdasdad"
+		src="https://picsum.photos/192?random=1"
+		class="p-2 pb-8 bg-white w-52 h-64 object-cover hidden lg:block -rotate-12 translate-y-3 z-10 shadow shadow-darker"
+	/>
+</section>
+
+<p>
+	Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad
+	nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+	Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla
+	est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+	reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident
+	adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
+	commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
+	consectetur et est culpa et culpa duis.
+</p>
+
+<h2>Random Facts</h2>
+
+<ul>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+</ul>
+
+<h2>Favorites</h2>
+
+<ul>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+	<li>
+		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+		cupidatat.
+	</li>
+</ul>
+
+<h2>Contact</h2>
+
+<p>
+	Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad
+	nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+	Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla
+	est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+	reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
+</p>
