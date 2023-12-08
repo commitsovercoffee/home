3e02349a8008841abc32e5d7b665ad773c0cce97
commitsovercoffee <commitsovercoffee@gmail.com>
2023-12-09 00:41:46 +0530

    refactors & styles nav bar

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,41 @@
+<script>
+	// get current page url
+	import { page } from '$app/stores';
+	$: currentURL = $page.url.pathname.split('/').pop() || $page.url.pathname;
+
+	// nav bar
+	let links = ['home', 'projects', 'blog', 'about'];
+	const style =
+		'inline m-2 border-dark p-2 uppercase no-underline font-contrail text-lg text-dark transition-color duration-300 ease-in-out';
+</script>
+
+<nav class="mb-24 flex justify-center">
+	{#each links as link}
+		{#if link === 'home'}
+			<a
+				class={`${style} ${
+					currentURL === '/'
+						? 'text-light border-b-2 border-light'
+						: 'hover:text-light hover:border-light'
+				}`}
+				href={`/`}
+			>
+				{link}
+			</a>
+		{:else if link === currentURL}
+			<a
+				class={`${style} ${link === currentURL && 'text-light border-b-2 border-light'}`}
+				href={link === 'home' ? '/' : `/${link}`}
+			>
+				{link}
+			</a>
+		{:else}
+			<a
+				class={`${style} hover:text-light hover:border-light`}
+				href={link === 'home' ? '/' : `/${link}`}
+			>
+				{link}
+			</a>
+		{/if}
+	{/each}
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,14 @@
 <script>
 	import '../app.css';
-
 	import '@fontsource/contrail-one';
 	import '@fontsource/ibm-plex-mono';
 	import '@fontsource/ibm-plex-sans';
 	import '@fontsource/ibm-plex-serif';
 	import '@fontsource-variable/caveat';
-
-	const links = ['Home', 'Projects', 'Blog', 'About'];
+	import Nav from '$lib/components/Nav.svelte';
 </script>
 
-<nav class="mb-24 flex justify-center">
-	{#each links as link}
-		<a href="/" class="inline p-2 mx-4 my-2 no-underline">{link}</a>
-	{/each}
-</nav>
+<Nav />
 <slot />
 <footer class="mt-24">
 	© commitsovercoffee | Made with <a href="https://svelte.dev/">svelte</a>.
