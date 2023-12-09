78da9c457aacb691610975fd626da8c85d8322cc
commitsovercoffee <commitsovercoffee@gmail.com>
2023-12-09 15:47:41 +0530

    updates about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,84 +2,80 @@
 
 <section class="flex justify-center items-center">
 	<img
-		alt="asdasdad"
+		alt="A cheerful dude who's surprisingly chill —     zero homicidal tendencies, just good vibes and laughter!"
 		src="https://picsum.photos/192?random=1"
-		class="p-2 pb-8 bg-white w-52 h-64 object-cover rotate-3 translate-x-3 shadow shadow-darker"
+		class="p-2 pb-8 bg-white w-52 h-64 object-cover rotate-3 translate-x-2 shadow shadow-darker text-darker"
 	/>
 	<img
-		alt="asdasdad"
+		alt="The same guy staring into the abyss, wondering if his dark side is just a metaphor for his questionable taste in socks."
 		src="https://picsum.photos/192?random=1"
-		class="p-2 pb-8 bg-white w-52 h-64 object-cover hidden lg:block -rotate-12 translate-y-3 z-10 shadow shadow-darker"
+		class="p-2 pb-8 bg-white w-52 h-64 object-cover hidden lg:block -rotate-12 translate-y-3 z-10 shadow shadow-darker text-darker"
 	/>
 </section>
 
 <p>
-	Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad
-	nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-	Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla
-	est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
-	reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident
-	adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
-	commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
-	consectetur et est culpa et culpa duis.
+	Hey folks! I'm Sourav. A minimalist, moody (more often than not), and mildly dramatic (even
+	sometimes, when maybe I shouldn't be).
+</p>
+
+<p>
+	I work as a software contractor. Which means I build software, consult, attend meetups and often
+	get to choose my tech stack :) And I work remotely, which means I get to do all of this from the
+	comfort of my PJs. I'm based out of Ranchi, but I often travel for meetups pan India. My hobbies
+	include reading psychological fiction, watching philosophy lectures, cycling, gaming and ricing my
+	linux machine.
+</p>
+
+<p>
+	I value a minimalist and thoughtful approach to software design and development, and I apply the
+	same principles whenever possible. Additionally, I have a keen interest in psychology and
+	education, and I relish participating in conversations and exploring ideas within these domains.
+</p>
+
+<p>
+	Feel free to reach out. You can send me a <a href="/">mail</a> or find me with my username ~
+	<span class="text-dark">commitsovercoffee</span> on
+	<a href="https://github.com/commitsovercoffee">github</a>,
+	<a href="https://www.youtube.com/@commitsovercoffee">youtube</a>
+	and discord.
+</p>
+<p>
+	PS You can find my resume <a href="/">here</a>.
 </p>
 
 <h2>Random Facts</h2>
 
 <ul>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
+	<li>I'm learning svelte and loving it!</li>
+	<li>I love tiling window managers, and tend to frown when not working in one.</li>
+	<li>I love strategy games and competetive MOBAs.</li>
+	<li>My courses on coding are taught in government schools across 3 Indian states.</li>
+	<li>In an alternate life, I would've been a drummer.</li>
 </ul>
 
 <h2>Favorites</h2>
 
 <ul>
+	<li>Music : Beatles, Led Zepplin, Arace Fire, A-ha & The Police.</li>
+
+	<li>Shows : The Silicon Valley, The Office, My Name is Earl & Rick and Morty.</li>
+
+	<li>Movies : Malena, Life is Beautiful, Clockwork Orange & Whiplash.</li>
+
 	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
+		Books : Animal Farm, 1984, Brave New World, Catch 22, Laughter in the dark, Notes from
+		Underground & Crime and Punishment.
 	</li>
+
 	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
-	</li>
-	<li>
-		Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-		cupidatat.
+		Games : Dota 2, Read Dead Redemption, The Witcher, The Spiderman, Assasin's Creed, God of War,
+		Tomb Raider, Hitman & Far Cry Primal.
 	</li>
 </ul>
 
-<h2>Contact</h2>
+<h2>Thanks</h2>
 
 <p>
-	Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad
-	nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-	Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla
-	est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
-	reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
+	Thank you for taking the time to get to know me. Feel free to reach out – whether it's for
+	collaboration, conversation, or just a virtual high-five. Here's to what lies ahead! 🍻
 </p>
